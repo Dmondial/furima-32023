4f92cebe9ad30ec340045bd16337be0ed4424890
Dmondial <www.ezweb.1ocofrank.acidman@gmail.com>
2020-11-29 15:08:47 +0900

    ログイン機能の実装、バリデーションの設定
    deviseのapp/views/sessions/new.html.erbに
    ログイン機能を実装（修正）
    
    userモデルファイルにバリデーションを設定
    (新規登録時の入力のバリデーション)

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,13 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, presence: true
-  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: 'Full-width characters' }
-  validates :first_name, presence: true,format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: 'Full-width characters' }
-  validates :last_name_kana, presence: true,format: { with: /\A[ァ-ン]+\z/, message: 'Full-width katakana characters' }
-  validates :first_name_kana, presence: true,format: { with: /\A[ァ-ン]+\z/, message: 'Full-width katakana characters' }
-  validates :birthday, presence: true
-  
+  with_options presence: true do
+    validates :nickname
+    validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: 'Full-width characters' }
+    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: 'Full-width characters' }
+    validates :last_name_kana, format: { with: /\A[ァ-ン]+\z/, message: 'Full-width katakana characters' }
+    validates :first_name_kana, format: { with: /\A[ァ-ン]+\z/, message: 'Full-width katakana characters' }
+    validates :birthday
+  end
+  validates :password, format: {with:/\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: 'Include both letters and numbers'}
 end
