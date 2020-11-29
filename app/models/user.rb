3f0bb50a8c1f4f15e168f419ac6717aa092eb0b0
Dmondial <www.ezweb.1ocofrank.acidman@gmail.com>
2020-11-29 10:44:10 +0900

    配布CSSファイルを配置

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,11 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  validates :nickname, presence: true
+  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: '全角文字を使用してください' }
+  validates :first_name, presence: true,format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: '全角文字を使用してください' }
+  validates :last_name_kana, presence: true,format: { with: /\A[ァ-ン]+\z/, message: '全角カタカナを使用してください' }
+  validates :first_name_kana, presence: true,format: { with: /\A[ァ-ン]+\z/, message: '全角カタカナを使用してください' }
+  validates :birthday, presence: true
+  
 end
