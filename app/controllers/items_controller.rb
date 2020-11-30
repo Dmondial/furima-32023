a94950778907806e082e33e58c358977bc43fe00
Dmondial <www.ezweb.1ocofrank.acidman@gmail.com>
2020-11-30 17:00:40 +0900

    商品出品ページからテーブルに保存される事を確認
    item_controllerにnew,createアクションを定義
    ストロングパラメーターを記述
    ActiveStorageのアソシエーションをitemモデルファイルに記述

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,24 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to items_path
+    else
+      render "/items/new"
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :price, :introduction, :category_id, :condition_id, :delivery_fee_id, :send_source_id, :send_time_id, :image).merge(user_id: current_user.id)
+  end
+
 end
