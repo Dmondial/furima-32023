dc1c654625dd8321935c4d612b091ee083631d5c
Dmondial <www.ezweb.1ocofrank.acidman@gmail.com>
2020-12-02 22:36:00 +0900

    PayJPを導入、購入機能実装

--- app/controllers/item_purchases_controller.rb
+++ app/controllers/item_purchases_controller.rb
@@ -8,6 +8,7 @@ class ItemPurchasesController < ApplicationController
   def create
     @purchase_form = PurchaseForm.new(item_purchase_params)
     if @purchase_form.valid?
+      pay_item
       @purchase_form.save
       redirect_to items_path
     else
@@ -18,6 +19,15 @@ class ItemPurchasesController < ApplicationController
   private
   def item_purchase_params
     params.require(:purchase_form).permit(:postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number)
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]  # 自身のPAY.JPテスト秘密鍵を記述しましょう
+    Payjp::Charge.create(
+      amount: Item.find(params[:item_id]).price,  # 商品の値段
+      card: params[:token],    # カードトークン
+      currency: 'jpy'                # 通貨の種類（日本円）
+    )
   end
 
 end
