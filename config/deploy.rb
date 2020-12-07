8be2c22eea71b4abaa0e0e6854a0d9e4a4894f78
Dmondial <www.ezweb.1ocofrank.acidman@gmail.com>
2020-12-07 22:12:45 +0900

    訂正

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,17 +1,12 @@
-# config valid for current version and patch releases of Capistrano
-lock "~> 3.14.1"
-
-set :application, "furima-32023"
-set :repo_url, "git@github.com:Dmondial/furima-32023.git"
-
+	
 # capistranoのバージョンを記載。固定のバージョンを利用し続け、バージョン変更によるトラブルを防止する
-# lock 'Capistranoのバージョン'
+lock '3.14.1'
 
-# # Capistranoのログの表示に利用する
-# set :application, 'ご自身のアプリケーション名'
+# Capistranoのログの表示に利用する
+set :application, 'furima-32023'
 
-# # どのリポジトリからアプリをpullするかを指定する
-# set :repo_url,  'git@github.com:Githubのユーザー名/レポジトリ名.git'
+# どのリポジトリからアプリをpullするかを指定する
+set :repo_url,  'git@github.com:Dmondial/furima-32023.git'
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
@@ -37,36 +32,3 @@ namespace :deploy do
     invoke 'unicorn:restart'
   end
 end
-# Default branch is :master
-# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
-
-# Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, "/var/www/my_app_name"
-
-# Default value for :format is :airbrussh.
-# set :format, :airbrussh
-
-# You can configure the Airbrussh format using :format_options.
-# These are the defaults.
-# set :format_options, command_output: true, log_file: "log/capistrano.log", color: :auto, truncate: :auto
-
-# Default value for :pty is false
-# set :pty, true
-
-# Default value for :linked_files is []
-# append :linked_files, "config/database.yml"
-
-# Default value for linked_dirs is []
-# append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
-
-# Default value for default_env is {}
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
-
-# Default value for local_user is ENV['USER']
-# set :local_user, -> { `git config user.name`.chomp }
-
-# Default value for keep_releases is 5
-# set :keep_releases, 5
-
-# Uncomment the following to require manually verifying the host key before first deploy.
-# set :ssh_options, verify_host_key: :secure
